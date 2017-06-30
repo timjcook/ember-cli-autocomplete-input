19582568788c0d81feb508c73e347c1344dc8dad
Tim Cook <tc@papercloud.com.au>
2017-06-30 14:49:10 +1000

    update autocomplete result item to be able to handle computed properties

--- tests/integration/components/autocomplete-result-item-test.js
+++ tests/integration/components/autocomplete-result-item-test.js
@@ -1,5 +1,8 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+
+const { Object: EmberObject, defineProperty, computed } = Ember;
 
 moduleForComponent('autocomplete-result-item', 'Integration | Component | autocomplete result item', {
   integration: true
@@ -36,10 +39,7 @@ test('it renders the result item highlighted with default mappings', function(as
     name: 'Essendon Bombers',
     value: 'bombers'
   });
-  this.set('highlightedResult', {
-    name: 'Essendon Bombers',
-    value: 'bombers'
-  });
+  this.set('highlightedResult', this.get('result'));
   this.render(hbs`{{autocomplete-result-item result=result highlightedResult=highlightedResult resultName='name' resultValue='value'}}`);
 
   assert.equal(this.$('.autocomplete-result-item').text().trim(), this.get('result').name, 'it displays the autocomplete result correctly');
@@ -52,13 +52,64 @@ test('it renders the result item highlighted with custom mappings', function(ass
     teamName: 'Essendon Bombers',
     teamValue: 'bombers'
   });
-  this.set('highlightedResult', {
-    teamName: 'Essendon Bombers',
-    teamValue: 'bombers'
-  });
+  this.set('highlightedResult', this.get('result'));
   this.render(hbs`{{autocomplete-result-item result=result highlightedResult=highlightedResult resultName='teamName' resultValue='teamValue'}}`);
 
   assert.equal(this.$('.autocomplete-result-item').text().trim(), this.get('result').teamName, 'it displays the autocomplete result correctly');
+});
+
+test('it renders a result item with computed propeties and default mappings', function(assert) {
+  assert.expect(1);
+
+  let result = new EmberObject({});
+  defineProperty(result, 'name', computed(function() { return 'Essendon Bombers'; }));
+  defineProperty(result, 'value', computed(function() { return 'bombers'; }));
+  this.set('result', result);
+
+  this.render(hbs`{{autocomplete-result-item result=result resultName='name' resultValue='value'}}`);
+
+  assert.equal(this.$('.autocomplete-result-item').text().trim(), this.get('result').get('name'), 'it displays the autocomplete result correctly');
+});
+
+test('it renders a result item with computed propeties and custom mappings', function(assert) {
+  assert.expect(1);
+
+  let result = new EmberObject({});
+  defineProperty(result, 'teamName', computed(function() { return 'Essendon Bombers'; }));
+  defineProperty(result, 'teamValue', computed(function() { return 'bombers'; }));
+  this.set('result', result);
+
+  this.render(hbs`{{autocomplete-result-item result=result resultName='teamName' resultValue='teamValue'}}`);
+
+  assert.equal(this.$('.autocomplete-result-item').text().trim(), this.get('result').get('teamName'), 'it displays the autocomplete result correctly');
+});
+
+test('it renders a result item highlighted with computed properties and default mappings', function(assert) {
+  assert.expect(1);
+
+  let result = new EmberObject({});
+  defineProperty(result, 'name', computed(function() { return 'Essendon Bombers'; }));
+  defineProperty(result, 'value', computed(function() { return 'bombers'; }));
+  this.set('result', result);
+  this.set('highlightedResult', this.get('result'));
+
+  this.render(hbs`{{autocomplete-result-item result=result highlightedResult=highlightedResult resultName='name' resultValue='value'}}`);
+
+  assert.equal(this.$('.autocomplete-result-item').text().trim(), this.get('result').get('name'), 'it displays the autocomplete result correctly');
+});
+
+test('it renders a result item highlighted with computed properties and custom mappings', function(assert) {
+  assert.expect(1);
+
+  let result = new EmberObject({});
+  defineProperty(result, 'teamName', computed(function() { return 'Essendon Bombers'; }));
+  defineProperty(result, 'teamValue', computed(function() { return 'bombers'; }));
+  this.set('result', result);
+  this.set('highlightedResult', this.get('result'));
+
+  this.render(hbs`{{autocomplete-result-item result=result highlightedResult=highlightedResult resultName='teamName' resultValue='teamValue'}}`);
+
+  assert.equal(this.$('.autocomplete-result-item').text().trim(), this.get('result').get('teamName'), 'it displays the autocomplete result correctly');
 });
 
 test('it fires the select result on click with default mappings', function(assert) {
