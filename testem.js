15fb74830e0120377030449d7061ae5c3bf0f4ae
Tim Cook <tc@papercloud.com.au>
2018-05-11 13:53:24 +1000

    update to ember 2.13

--- testem.js
+++ testem.js
@@ -1,19 +1,12 @@
 /* eslint-env node */
 module.exports = {
-  test_page: "tests/index.html?hidepassed",
-  disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "PhantomJS"
   ],
-  launch_in_dev: [
-    'Chrome'
-  ],
-  browser_args: {
-    Chrome: [
-      '--disable-gpu',
-      '--headless',
-      '--remote-debugging-port=9222',
-      '--window-size=1440,900'
-    ]
-  }
+  "launch_in_dev": [
+    "PhantomJS",
+    "Chrome"
+  ]
 };
